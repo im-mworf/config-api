9029e499dd53e5b8d8e66f6bfdea4961424bd000
im-worf <marcus.worf@immediate.co.uk>
2023-12-05 16:57:20 +0000

    add: item state provider & processor

---
+++ api/src/State/ItemStateProcessor.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\DeleteOperationInterface;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\State\ProcessorInterface;
+use App\Entity\Item;
+use App\ApiResource\Item as ItemResource;
+use App\Mapper\EntityMapper;
+use App\Repository\ItemRepository;
+
+readonly class ItemStateProcessor implements ProcessorInterface
+{
+    public function __construct(
+        private ItemRepository $itemRepository,
+        private ItemStateProvider $itemProvider,
+        private EntityMapper $entityMapper
+    ) {
+    }
+
+    /**
+     * @throws \ReflectionException
+     */
+    public function process(
+        mixed $data,
+        Operation $operation,
+        array $uriVariables = [],
+        array $context = []
+    ) {
+        // check we are dealing with the correct resource
+        assert($data instanceof ItemResource);
+
+        // find resource if we can (for PUT/PATCH/DELETE)
+        $item = $this->itemRepository->findOneBy($uriVariables);
+
+        // if we are deleting then handle the delete process
+        if ($operation instanceof DeleteOperationInterface) {
+            if ($item !== null) {
+                $this->itemRepository->remove($item);
+            }
+
+            return null;
+        }
+
+        // if we are posting create a new entity
+        if ($operation instanceof Post) {
+            $item = new Item();
+        }
+
+        // use the repository to persist the entity returned from the mapper
+        // you have full control here - the mapper is used for same
+        // named resource/entity properties for convenience
+        $this->itemRepository->persist($this->entityMapper->map($item, $data));
+
+        // re-affirm the key (for POST) (unique case as we are explicitly setting the identifier)
+        $uriVariables['key'] = $data->key;
+
+        // return the item from the provider
+        return $this->itemProvider->provide($operation, $uriVariables, $context);
+    }
+}
---
+++ api/src/State/ItemStateProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use App\ApiResource\Item as ItemResource;
+use App\Mapper\ResourceMapper;
+use App\Repository\ItemRepository;
+
+readonly class ItemStateProvider implements ProviderInterface
+{
+    public function __construct(
+        private ItemRepository $itemRepository,
+        private ResourceMapper $resourceMapper
+    ) {
+    }
+
+    /**
+     * @throws \ReflectionException
+     */
+    public function provide(
+        Operation $operation,
+        array $uriVariables = [],
+        array $context = []
+    ): object|array|null {
+        // fetch the entity using the repository
+        $item = $this->itemRepository->findOneBy($uriVariables);
+
+        // return if entity is null/not found
+        if ($item === null) {
+            return null;
+        }
+
+        // map the entity to the api resource - lots of opportunity
+        // here we use a resource mapper for convenience
+        // return the resource - hydra takes care of the rest
+        return $this->resourceMapper->map(new ItemResource(), $item);
+    }
+}
