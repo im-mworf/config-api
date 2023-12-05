fcdf7f31b54536f42bb08ea6844cc5f27d3920a7
im-worf <marcus.worf@immediate.co.uk>
2023-12-05 16:55:01 +0000

    add: item api resource

---
+++ api/src/ApiResource/Item.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\ApiResource;
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\State\ItemStateProcessor;
+use App\State\ItemStateProvider;
+
+#[ApiResource(
+    operations: [
+        new Get(),
+        new Post(),
+        new Put(),
+        new Patch(),
+        new Delete(),
+    ],
+    provider: ItemStateProvider::class,
+    processor: ItemStateProcessor::class
+)]
+class Item implements ResourceInterface
+{
+    #[ApiProperty(readable: true, identifier: true)]
+    public string $key;
+
+    #[ApiProperty(readable: true)]
+    public string $name;
+
+    #[ApiProperty(readable: true)]
+    public ?string $value;
+}
